7875511a1d01b6580e5ebdb22b291ec2b2db8617
Sang min Leee <dltkdals12345@gmail.com>
2026-09-03 09:17:28 +0900

    solve: 프로그래머스 42895 N으로 표현 - 사용 횟수별 집합을 구성하는 동적 계획법 적용 - 사칙연산과 N 반복 연결 숫자를 생성해 중복 제거

--- coding-test/문제/프로그래머스/3단계/프로그래머스_42895_N으로_표현.java
+++ coding-test/문제/프로그래머스/3단계/프로그래머스_42895_N으로_표현.java
@@ -1,50 +1,76 @@
-package algo_workshop;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-class Solution_N으로표현 {
-
+class Solution {
+	static List<int[]> combination(int N) {
+		List<int[]> list = new ArrayList<>();
+		
+		for (int i=1; i<N; i++) {
+			int[] arr = {i, N-i};
+			list.add(arr);
+		}		
+		return list;
+	}
     public int solution(int N, int number) {
-        Set<Integer>[] dp = new HashSet[9];
-        
-        for(int i=1; i<=8; i++) {
-        	dp[i] = new HashSet<>();
+        int answer = 0;
+        int[][] arr = new int [8][];
+        int[] first = {N};
+        arr[0] = first;        
+        if (N==number) {
+        	return 1;
+        }
+        for (int i=1; i<8; i++) {
+        	Set<Integer> set = new HashSet<>();
+        	List<int[]> list = combination(i+1);
         	
-        	int repeated = 0;
-        	for (int k=0; k<i; k++) {
-        		repeated = repeated * 10 + N;
-        	}
-        	
-        	dp[i].add(repeated);
-        	
-        	for (int j=1; j<i; j++) {
-        		for (int a: dp[j]) {
-        			for (int b: dp[i-j]) {
-        				dp[i].add(a + b);
-        				dp[i].add(a - b);
-        				dp[i].add(a * b);
+        	for(int j=0; j<list.size(); j++) {
+        		int[] idx = list.get(j);
+        		for(int k=0; k<arr[idx[0] - 1].length; k ++) {
+        			for(int l=0; l<arr[idx[1] - 1].length; l++) {
+        				int n = arr[idx[0] - 1][k];
+        				int m = arr[idx[1] - 1][l];
         				
-        				if (b != 0) {
-        					dp[i].add(a / b);
+        				if(n==0 || m==0) {
+        					continue;
+        				}
+        				
+        				set.add(n + m);
+        				set.add(n * m);
+        				
+        				if (n >= m) {
+        					set.add(n - m);
+        					if (n%m == 0) {
+        						set.add(n/m);
+        					}
         				}
         			}
         		}
         	}
-        	if (dp[i].contains(number)) {
-        		return i;
-        	}
+        	
+        	int cont = 0;
+        	for (int k=0; k<=i; k++) {
+    			cont += N * Math.pow(10, k);
+    		}
+
+    		set.add(cont);
+    		
+//    		for(int s: set) {
+//    			System.out.print(s + " ");
+//    		}
+//    		System.out.println();
+        	
+        	arr[i] = set.stream()
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        	
+        	if (set.contains(number)) {
+        		answer = i;
+        		return answer + 1;
+    		}
         }
+        
         return -1;
     }
 }
-
-public class Main_N으로표현 {
-	public static void main(String[] args) {
-		Solution_N으로표현 s = new Solution_N으로표현();
-		System.out.println(s.solution(5, 5));
-	}
-}
- 
